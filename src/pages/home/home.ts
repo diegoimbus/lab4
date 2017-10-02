71c58d8e6353a9764e6455acae193340205e8c39
Diego Imbus <dsn.imbus01@gmail.com>
2017-10-02 08:40:48 -0500

    login interface

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { LoginPage } from '../login/login';
+import { Storage } from '@ionic/storage';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  root: any = LoginPage;
+  constructor(public navCtrl: NavController, public storage: Storage) {
+
+  }
+  setContent(index:number){
+    if(index == 0) {
+      this.root = HomePage;
+    }
+    else {
+      this.root = LoginPage;
+    }
+  }
+
+  logout(){
+    this.storage.set("logged", false)
+    this.navCtrl.setRoot(LoginPage)
 
   }
 
+}
+
+interface Menu {
+  label: string;
+  icon: string;
 }
